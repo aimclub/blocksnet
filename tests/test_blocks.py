a1e7a18867241045c46dd2e2240af370cd664d91
vasilstar <vasilstar97@gmail.com>
2023-09-08 17:35:48 +0300

    feat: initial testing

---
+++ tests/test_blocks.py
@@ -0,0 +1,59 @@
+"""Testing blocks cutter behavior"""
+
+import os
+import pytest
+import geopandas as gpd
+from masterplan_tools.method.blocks import BlocksCutter, CutParameters, LandUseParameters
+
+data_path = "./tests/data/blocks"
+local_crs = 32636
+blocks_count = 1209
+
+
+@pytest.fixture
+def city_geometry():
+    return gpd.read_parquet(os.path.join(data_path, "city_geometry.parquet")).to_crs(local_crs)
+
+
+@pytest.fixture
+def cut_params(city_geometry):
+    water_geometry = gpd.read_parquet(os.path.join(data_path, "water_geometry.parquet")).to_crs(local_crs)
+    roads_geometry = gpd.read_parquet(os.path.join(data_path, "roads_geometry.parquet")).to_crs(local_crs)
+    railways_geometry = gpd.read_parquet(os.path.join(data_path, "railways_geometry.parquet")).to_crs(local_crs)
+    return CutParameters(city=city_geometry, water=water_geometry, roads=roads_geometry, railways=railways_geometry)
+
+
+@pytest.fixture
+def lu_params():
+    no_development = gpd.read_parquet(os.path.join(data_path, "no_development.parquet")).to_crs(local_crs)
+    landuse = gpd.read_parquet(os.path.join(data_path, "landuse.parquet")).to_crs(local_crs)
+    buildings = gpd.read_parquet(os.path.join(data_path, "buildings_geom.parquet")).to_crs(local_crs)
+    return LandUseParameters(no_development=no_development, landuse=landuse, buildings=buildings)
+
+
+@pytest.fixture
+def blocks(cut_params, lu_params):
+    return BlocksCutter(cut_parameters=cut_params, lu_parameters=lu_params).get_blocks()
+
+
+def test_count(blocks):
+    """Check if our blocks count doesn't change"""
+    assert len(blocks.to_gdf()) == blocks_count
+
+
+def test_intersection(blocks):
+    """Check if some of blocks intersect others"""
+    assert (
+        len(
+            gpd.sjoin(blocks.to_gdf(), blocks.to_gdf(), how="inner", predicate="intersects").loc[
+                lambda x: x["id_left"] != x["id_right"]
+            ]
+        )
+        == 0
+    )
+
+
+def test_within(blocks, city_geometry):
+    """Check if city blocks are inside initial city geometry with buffer"""
+    blocks_gdf = blocks.to_gdf()
+    assert blocks_gdf["geometry"].apply(lambda geom: city_geometry["geometry"].buffer(10).contains(geom))[0].all()
---
+++ tests/test_preprocessing.py
@@ -0,0 +1,50 @@
+"""Testing blocks cutter behavior"""
+
+import os
+import pytest
+import geopandas as gpd
+from masterplan_tools.preprocessing import DataGetter, AggregateParameters
+
+data_path = "./tests/data/preprocessing"
+local_crs = 32636
+
+
+@pytest.fixture
+def cutted_blocks():
+    return gpd.read_parquet(os.path.join(data_path, "cutted_blocks.parquet")).to_crs(local_crs)
+
+
+@pytest.fixture
+def getter(cutted_blocks):
+    return DataGetter(blocks=cutted_blocks)
+
+
+@pytest.fixture
+def aggr_params():
+    buildings = gpd.read_parquet(os.path.join(data_path, "buildings.parquet")).to_crs(local_crs)
+    greenings = gpd.read_parquet(os.path.join(data_path, "greenings.parquet")).to_crs(local_crs)
+    parkings = gpd.read_parquet(os.path.join(data_path, "parkings.parquet")).to_crs(local_crs)
+
+    return AggregateParameters(buildings=buildings, greenings=greenings, parkings=parkings)
+
+
+@pytest.fixture
+def aggr_blocks(getter, aggr_params):
+    return getter.aggregate_blocks_info(params=aggr_params)
+
+
+def test_count(cutted_blocks, aggr_blocks):
+    """Check if we receive as much blocks as we give"""
+    assert len(cutted_blocks) == len(aggr_blocks.to_gdf())
+
+
+def test_ids(aggr_blocks):
+    """Check if IDs are unique and equal to index"""
+    gdf = aggr_blocks.to_gdf()
+    assert len(gdf["block_id"].unique()) == len(gdf)
+    assert (gdf.index == gdf["block_id"]).all()
+
+
+# def test_area(aggr_blocks):
+#   gdf = aggr_blocks.to_gdf()
+#   assert (gdf['area'] >= (gdf['current_green_area'] + gdf['current_industrial_area'] + gdf['current_living_area'])).all()
