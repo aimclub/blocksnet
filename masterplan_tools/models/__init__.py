0559326122913fc5899fd3f84e4c0af8e7c8d0eb
vasilstar <vasilstar97@gmail.com>
2023-09-22 01:24:21 +0300

    feat: service type can be added, service type layer can be updated

--- masterplan_tools/models/__init__.py
+++ masterplan_tools/models/__init__.py
@@ -3,4 +3,4 @@ Models classes are located here.
 """
 from .city_model import CityModel
 from .geojson import GeoJSON, PointGeoJSON, PolygonGeoJSON
-from .block import Block
+from .city import City, Block, Service, ServiceType
