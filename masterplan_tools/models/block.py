cd6989cb6a77cee1753b4f7879ccccf72d5bc34a
Vasilii Starikov <vasilstar97@gmail.com>
2023-09-21 14:17:01 +0300

    graph creation example with blocks as nodes

--- masterplan_tools/models/block.py
+++ masterplan_tools/models/block.py
@@ -7,7 +7,7 @@ class Block(BaseModel):
     id: int
     geometry: InstanceOf[Polygon]
     population: int = Field(ge=0)
-    floors: int = Field(ge=0)
+    floors: float = Field(ge=0)
     area: float = Field(ge=0)
     living_area: float = Field(ge=0)
     green_area: float = Field(ge=0)
@@ -20,9 +20,23 @@ class Block(BaseModel):
         return self.population > 0
 
     @classmethod
-    def from_gdf(gdf):
-
-        return
+    def from_gdf(cls, gdf: gpd.GeoDataFrame) -> "list[Block]":
+        return (
+            gdf.rename(
+                columns={
+                    "block_id": "id",
+                    "current_population": "population",
+                    "current_living_area": "living_area",
+                    "current_green_capacity": "green_capacity",
+                    "current_green_area": "green_area",
+                    "current_parking_capacity": "parking_capacity",
+                    "current_industrial_area": "industrial_area",
+                },
+                inplace=False,
+            )
+            .apply(lambda x: cls(**x.to_dict()), axis=1)
+            .to_list()
+        )
 
     def __hash__(self):
         return hash(self.id)
