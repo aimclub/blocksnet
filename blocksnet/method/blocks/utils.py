ce2c709eac169cae34837f87cf796d01e4d5d8a0
Леонид Харлов <harlov.leonid@ya.ru>
2023-12-01 18:28:29 +0300

    add functions from blocks_generator to utils

--- blocksnet/method/blocks/utils.py
+++ blocksnet/method/blocks/utils.py
@@ -1,86 +1,138 @@
 import geopandas as gpd
 from shapely.geometry import MultiPolygon, Polygon
-from functools import reduce
+
+import pyproj
+from pyproj.aoi import AreaOfInterest
+from pyproj.database import query_crs_info
+from pyproj import CRS
 
 
-class Utils:
-    @staticmethod
-    def _fill_spaces_in_blocks(block: gpd.GeoSeries) -> Polygon:
-        """
-        This geometry will be cut later from city's geometry.
-        The water entities will split blocks from each other. The water geometries are taken using overpass turbo.
-        The tags used in the query are: "natural"="water", "waterway"~"river|stream|tidal_channel|canal".
-        This function is designed to be used with 'apply' function, applied to the pd.DataFrame.
+def verbose_print(text, verbose=True):
+    if verbose: print(text)
 
 
-        Parameters
-        ----------
-        row : GeoSeries
+def get_projected_crs(poly):
+    """
+    Returns a local projected CRS for a given polygon.
+    If no matching local CRS found, returns EPSG:3857.
+    
+    Attributes
+    ----------
+    poly: shapely.Polygon or shapely.MultiPolygon
+        A polygon of a place.
+        
+    Returns
+    -------
+    projected_crs: pyproj.CRS
+        Projected CRS.
+    """
+    
+    try:
+        coords = [list(set(x)) for x in poly.envelope.boundary.coords.xy]
 
-
-        Returns
-        -------
-        water_geometry : Union[Polygon, Multipolygon]
-            Geometry of water. The water geometries are also buffered a little so the division of city's geometry
-            could be more noticable.
-        """
-
-        new_block_geometry = None
-
-        if len(block["rings"]) > 0:
-            empty_part_to_fill = [Polygon(ring) for ring in block["rings"]]
-
-            if len(empty_part_to_fill) > 0:
-                new_block_geometry = reduce(
-                    lambda geom1, geom2: geom1.union(geom2), [block["geometry"]] + empty_part_to_fill
-                )
-
-        if new_block_geometry:
-            return new_block_geometry
-
-        return block["geometry"]
-
-    @staticmethod
-    def _fix_blocks_geometries(city_geometry):
-        """
-        After cutting several entities from city's geometry, blocks might have unnecessary spaces inside them.
-        In order to avoid this, this functions prepares data to fill empty spaces inside each city block
-
-        Returns
-        -------
-        None
-        """
-
-        city_geometry = city_geometry.explode(ignore_index=True)
-        city_geometry["rings"] = city_geometry.interiors
-        city_geometry["geometry"] = city_geometry[["geometry", "rings"]].apply(
-            Utils._fill_spaces_in_blocks, axis="columns"
+        area_of_interest = AreaOfInterest(
+            west_lon_degree=coords[0][0],
+            east_lon_degree=coords[0][1],
+            north_lat_degree=coords[1][0],
+            south_lat_degree=coords[1][1],
         )
 
-        return city_geometry
+        utm_crs_list = query_crs_info(
+            pj_types=pyproj.enums.PJType.PROJECTED_CRS,
+            area_of_interest=area_of_interest,
+        )
+        
+        projected_crs = CRS.from_epsg(utm_crs_list[0].code)
+        
+    except:
+        projected_crs = CRS(3857)
+    
+    return projected_crs
 
-    @staticmethod
-    def polygon_to_multipolygon(gdf: gpd.GeoDataFrame) -> gpd.GeoDataFrame:
-        """
-        This function makes one multipolygon from many polygons in the gdf.
-        This step allows to fasten overlay operation between two multipolygons since overlay operates ineratively
-        by passed geometry objects.
+def fill_holes(gdf):
+    """
+    Fills holes in geometries of a given GeoDataFrame
+    
+    Attributes
+    ----------
+    gdf: gpd.GeoDataFrame
+        GeoDataFrame with geometries to be filled.
+        
+    Returns
+    -------
+    gdf: gpd.GeoDataFrame
+        GeoDataFrame with filled geometries.
+    """
+    
+    gdf["geometry"] = gdf["geometry"].boundary
+    gdf = gdf.explode(index_parts=False)
+    gdf["geometry"] = gdf["geometry"].map(
+        lambda x: Polygon(x) if x.geom_type != 'Point' else np.nan)
+    gdf = gdf.dropna(subset='geometry').reset_index(drop=True).to_crs(4326)
+    
+    return gdf
 
-        Attributes
-        ----------
-        gdf: gpd.GeoDataFrame
-            A gdf with many geometries-polygons
+def drop_contained_geometries(gdf):
+    """
+    Drops geometries that are contained inside other geometries.
+    
+    Attributes
+    ----------
+    gdf: gpd.GeoDataFrame
+        GeoDataFrame with contained geometries.
+        
+    Returns
+    -------
+    gdf: gpd.GeoDataFrame
+        GeoDataFrame without contained geometries.
+    """
+    
+    gdf = gdf.reset_index(drop=True)
+    
+    overlaps = gdf["geometry"].sindex.query(gdf["geometry"], predicate="contains")
+    contains_dict = {x:[] for x in overlaps[0]}
+    for x, y in zip(overlaps[0], overlaps[1]):
+        if x != y:
+            contains_dict[x].append(y)
+            
+    contained_geoms_idxs = list({x for v in contains_dict.values() for x in v})
+    gdf = gdf.drop(contained_geoms_idxs)
+    gdf = gdf.reset_index(drop=True)
+    
+    return gdf
 
-        Returns
-        -------
-        gdf: Multipolygon
-            A gdf with one geometry-MultiPolygon
-        """
-
-        crs = gdf.crs
-        gdf = gdf.unary_union
-        if isinstance(gdf, Polygon):
-            gdf = gpd.GeoDataFrame(geometry=[gdf], crs=crs)
-        else:
-            gdf = gpd.GeoDataFrame(geometry=[MultiPolygon(gdf)], crs=crs)
-        return gdf
+def filter_bottlenecks(gdf,projected_crs,min_width=40):
+    """
+    Divides geometries in narrow places and removes small geometries.
+    
+    Attributes
+    ----------
+    gdf: gpd.GeoDataFrame
+        GeoDataFrame with contained geometries.
+    
+    projected_crs: int or pyproj.CRS
+        Metric projected CRS of a given area.
+        
+    min_width: int or float
+        Minimum allowed width of geometries in resulting GeoDataFrame.
+    
+    Returns
+    -------
+    gdf: gpd.GeoDataFrame
+        GeoDataFrame with processed geometries.
+    """
+    
+    def _filter_bottlenecks_helper(poly,min_width=40):
+        try: return poly.intersection(poly.buffer(-min_width/2).buffer(min_width/2,join_style=2))
+        except: return poly
+    
+    gdf['geometry'] = gdf.to_crs(projected_crs)['geometry'].map(
+        lambda x: _filter_bottlenecks_helper(x,min_width)).set_crs(projected_crs).to_crs(4326)
+    gdf = gdf[gdf['geometry'] != Polygon()]
+    gdf = gdf.explode(index_parts=False)
+    gdf = gdf[gdf.type == 'Polygon']
+    
+    if 'area' in gdf.columns:
+        gdf['area'] = gdf.to_crs(projected_crs).area
+        
+    return gdf
