99db6ffc6afd7a45c5028ac72d51a5f42c86e872
kryksh <kryksh@github.com>
2023-12-04 12:19:55 +0300

    fix genetic

--- blocksnet/method/genetic/template.py
+++ blocksnet/method/genetic/template.py
@@ -3,49 +3,132 @@ import pandas as pd
 from ..base_method import BaseMethod
 from ...models import Block
 from ..provision import Provision
+from pydantic import Field, InstanceOf
+import itertools
+import pygad
+from blocksnet.utils.measurement_units import HECTARE_IN_SQUARE_METERS
 
 
 class Genetic(BaseMethod):
-    # у тебя здесь уже есть city_model из BaseMethod, можешь обращаться к ней через self.city_model
-    # а инициализировать как gen = Genetic(city_model=city_model)
+    BLOCKS: InstanceOf[pd.DataFrame] = pd.DataFrame()
+    PROVISION: InstanceOf[Provision] = None
+    SCENARIO: dict
+    BUILDING_OPTIONS: InstanceOf[pd.DataFrame] = pd.DataFrame()
+    GA_PARAMS: dict = {
+        "num_generations": 2,
+        "num_parents_mating": 6,
+        "sol_per_pop": 10,
+        "mutation_type": "adaptive",
+        "mutation_percent_genes": (90, 10),
+        "crossover_type": "scattered",
+        "parent_selection_type": "tournament",
+        "K_tournament": 3,
+        "stop_criteria": "saturate_50",
+        "parallel_processing": 12,
+        "keep_parents": 1,
+    }
 
-    # инит тебе переписывать не нужно (и даже нельзя) потому что это уже делает BaseModel в BaseMethod (см. pydantic)
+    def flatten_dict(self, services: dict) -> tuple[dict[str, float], pd.DataFrame]:
+        """Utility function for get flatten dictionary of services requriments"""
+        services_dict = {}
+        for service, requirements in services.items():
+            if service not in self.SCENARIO:
+                continue
+            for population, area in requirements.items():
+                services_dict[service + "_" + str(population)] = area
+        return services_dict, pd.DataFrame([services_dict]).T
 
-    # это твой входной метод, юзер просто хочет его вызвать и дать туда какие-то ограничения на количество кварталов или что-то еще что ты придумаешь
-    # главное максимально инкапсулировать его на уровень "я хочу обновить три любых квартала" или "вот мои три квартала обнови их" в общем на твое усмотрение:
-    # gen.calculate(scenario) - это например для всех кварталов
-    def calculate(
-        self, scenario, selected_blocks: list[Block] = []
-    ) -> gpd.GeoDataFrame:  # верни здесь какой-нибудь гдф или дф который будет содержать "постройку" сервиса и мощности
-        """
-        Если selected_blocks тебе не отдали, то делай оптимизацию по всему городу
-        selected_blocks принимает не айдишники кварталов, а сами кварталы, их ты можешь выцепить через city_model.blocks (это прям экземпляры класса Block)
-        поэтому ты из этих Block можешь выцепить всю нужную инфу (смотри class Block в models/city) там есть area и прочее
-        ----
-        Scenario принимай как было раньше:
-        {'schools':0.5, 'kindergartens':0.5} и т.д.
-        """
-        city_model = self.city_model  # город у тебя уже инициализирован
-        blocks = self.city_model.blocks  # можешь вот так выцепить список всех Block
-        blocks[0].area  # площадь квартала на нулевом месте в списке
-        city_model[0].area  # площадь квартала с id=0
-        prov = Provision(
-            city_model=city_model
-        )  # вот твой стабильный пров через который считается обеспеченность и который уже знает свой город
-        #
-        #
-        # как задавать апдейт по кварталам (если неудобно или будут идеи лучше, предлагай, я ориентировался на то что по стандарту мы работаем в дф/гдф)
-        update = {
-            148: {
-                "population": 1000,
-            },
-            150: {"schools": 1000},
+    def get_combinations(self, services_dict: dict, comb_len: int) -> list:
+        """Determination of all possible combinations of services in the blocks, depending on the scenario"""
+        return [
+            item
+            for sublist in [list(itertools.combinations(list(services_dict.keys()), i)) for i in range(1, comb_len + 1)]
+            for item in sublist
+        ]
+
+    def get_combinations_area(self, combinations: list, services_df: pd.DataFrame) -> list:
+        """Calculation area of services for combinations"""
+        return [services_df.loc[list(combination)].sum()[0] for combination in combinations]
+
+    def updating_blocks_combinations(self, combinations_weights):
+        """Updating the block dataframe with possible combinations depending on the free area"""
+        self.BLOCKS["variants"] = self.BLOCKS["free_area"].apply(
+            lambda free_area: [
+                i for i, combination_weight in enumerate(combinations_weights) if combination_weight <= free_area
+            ]
+        )
+
+    def get_building_options(self, combinations: list):
+        """Filtering unsuitable combinations and updating all possible"""
+        total_building_options = list(set([item for sublist in self.BLOCKS["variants"].tolist() for item in sublist]))
+        combinations = [combinations[i] for i in total_building_options]
+        self.BUILDING_OPTIONS = pd.DataFrame(
+            columns=list(self.SCENARIO.keys()), index=range(len(total_building_options))
+        ).fillna(0)
+        for i, _ in enumerate(combinations):
+            for el in [x.rsplit("_", maxsplit=1) for x in _]:
+                self.BUILDING_OPTIONS.loc[i, el[0]] += int(el[1])
+        self.BUILDING_OPTIONS.index = total_building_options
+
+    def get_updated_blocks(self, building_options_ids, blocks_ids=None):
+        """Get updated blocks with calculated provision"""
+        updated_blocks = self.BUILDING_OPTIONS.loc[building_options_ids]
+        if blocks_ids:
+            updated_blocks.index = blocks_ids
+        else:
+            updated_blocks.index = self.BLOCKS["id"]
+
+        return updated_blocks.to_dict("index")
+
+    def fitness_func(self, ga_instance, solution, solution_idx):
+        """Fitness function for genetic algorithm"""
+        updated_blocks = pd.DataFrame.from_dict((self.get_updated_blocks(solution)), orient="index")
+        provisions, fitness = self.PROVISION.calculate_scenario(self.SCENARIO, updated_blocks)
+        return fitness
+
+    def get_blocks(self):
+        data = []
+        for block in self.city_model.blocks:
+            data.append({k: v for k, v in block})
+        gdf = gpd.GeoDataFrame(data).set_index("id").set_crs(epsg=self.city_model.epsg)
+        gdf["free_area"] = (
+            gdf["area"] * 0.8 - gdf["green_area"] - gdf["industrial_area"] - gdf["living_area"]
+        ) / HECTARE_IN_SQUARE_METERS
+        return gdf.reset_index()
+
+    def get_ga_params(self):
+        return {
+            "fitness_func": self.fitness_func,
+            "num_generations": self.GA_PARAMS["num_generations"],
+            "num_parents_mating": self.GA_PARAMS["num_parents_mating"],
+            "sol_per_pop": self.GA_PARAMS["sol_per_pop"],
+            "num_genes": self.BLOCKS.shape[0],
+            "gene_space": self.BLOCKS["variants"].tolist(),
+            "gene_type": int,
+            "parent_selection_type": self.GA_PARAMS["parent_selection_type"],
+            "keep_parents": self.GA_PARAMS["keep_parents"],
+            "crossover_type": self.GA_PARAMS["crossover_type"],
+            "mutation_type": self.GA_PARAMS["mutation_type"],
+            "mutation_percent_genes": self.GA_PARAMS["mutation_percent_genes"],
+            "K_tournament": self.GA_PARAMS["K_tournament"],
+            "stop_criteria": self.GA_PARAMS["stop_criteria"],
+            "parallel_processing": self.GA_PARAMS["parallel_processing"],
         }
-        update_df = pd.DataFrame.from_dict(update, orient="index")
-        # как считать провижен сценария: отдаешь ему свой scenario и дф с обновлениями, из пришедшего тупла заберешь вторую чиселку: total, её максимизируешь
-        _, total = prov.calculate_scenario(scenario, update_df)
 
-    def plot(
-        gdf: gpd.GeoDataFrame,
-    ):  # здесь по желанию метод, в который ты можешь отдать гдф из твоего calculate и нарисовать что-то на карте
-        ...
+    def calculate(self, services: dict, comb_len: int, selected_blocks: list[Block] = []) -> gpd.GeoDataFrame:
+        """Calculation of the optimal development option by services for blocks"""
+        self.BLOCKS = self.get_blocks()
+        self.PROVISION = Provision(city_model=self.city_model)
+        services_dict, services_df = self.flatten_dict(services)
+        combinations = self.get_combinations(services_dict, comb_len)
+        combinations_area = self.get_combinations_area(combinations, services_df)
+        self.updating_blocks_combinations(combinations_area)
+        self.BLOCKS = self.BLOCKS[self.BLOCKS["variants"].apply(lambda x: len(x)) != 0]
+        self.get_building_options(combinations)
+
+        self.GA_PARAMS = self.get_ga_params()
+        ga_instance = pygad.GA(**self.GA_PARAMS)
+        ga_instance.run()
+        solution, solution_fitness, solution_idx = ga_instance.best_solution()
+        updated_blocks = self.get_updated_blocks(solution)
+        return updated_blocks
