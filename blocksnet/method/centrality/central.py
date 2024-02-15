862bed648dc135e9356a9bc187bb43acbab98467
Mvin8 <skitless1229@gmail.com>
2024-02-15 18:40:28 +0500

    Dostrigi for vacant area and centrality

--- blocksnet/method/centrality/central.py
+++ blocksnet/method/centrality/central.py
@@ -15,38 +15,69 @@ from ..connectivity import Connectivity
 
 class CentralPlace(BaseMethod):
 
-    def count_points_in_polygon(self, polygon, points):
+    def count_points_in_polygon(self, polygon, points) -> int:
+        """
+        Count the number of points within a given polygon.
+
+        Parameters:
+        - polygon: A polygon geometry within which points are counted.
+        - points: A GeoDataFrame of point geometries to be tested against the polygon.
+
+        Returns:
+        - An integer count of points within the polygon.
+        """
         return points.within(polygon).sum()
 
+    def _shannon_diversity_index(self, counts) -> float:
+        """
+        Calculate the Shannon diversity index given counts.
 
-    def calculate_shannon_diversity(self, points, polygons):
+        Parameters:
+        - counts: A pandas Series representing counts of different categories.
 
+        Returns:
+        - The Shannon diversity index as a float.
+        """
+        proportions = counts / counts.sum()
+        proportions = proportions[proportions > 0]
+        return -np.sum(proportions * np.log(proportions))
+
+    def calculate_shannon_diversity(self, points, polygons) -> gpd.GeoDataFrame:
+        """
+        Calculate Shannon diversity index for each polygon based on point data.
+
+        Parameters:
+        - points: A GeoDataFrame of point geometries.
+        - polygons: A GeoDataFrame of polygon geometries.
+
+        Returns:
+        - A GeoDataFrame of polygons with an additional 'shannon' column representing the Shannon diversity index.
+        """
         joined_gdf = gpd.sjoin(points, polygons, how="inner", op="within")
-
         counts_per_polygon = joined_gdf.groupby(['index_right', 'city_service_type']).size().unstack(fill_value=0)
-
-        def shannon_diversity_index(counts):
-            # Расчет доли каждого типа услуги
-            proportions = counts / counts.sum()
-            # Использование только ненулевых долей для избежания деления на ноль
-            proportions = proportions[proportions > 0]
-            # Расчет индекса Шеннона
-            return -np.sum(proportions * np.log(proportions))
-
-        # Применение функции расчета индекса к каждой строке
-        shannon_indexes = counts_per_polygon.apply(shannon_diversity_index, axis=1)
-
+        shannon_indexes = counts_per_polygon.apply(self._shannon_diversity_index, axis=1)
         polygons['shannon'] = polygons.index.map(shannon_indexes)
-
         return polygons
 
-        
-    
     def connectivity(self) -> gpd.GeoDataFrame:
+        """
+        Calculate connectivity for the city model.
+
+        Returns:
+        - A GeoDataFrame representing connectivity metrics.
+        """
         return Connectivity(city_model=self.city_model).calculate()
 
+    def centrality(self, points) -> gpd.GeoDataFrame:
+        """
+        Calculate centrality metrics for polygons based on point data and connectivity.
 
-    def centrality(self, points):
+        Parameters:
+        - points: A GeoDataFrame of point geometries.
+
+        Returns:
+        - A GeoDataFrame of polygons with added centrality metrics.
+        """
         polygons = self.connectivity()
         polygons['density'] = polygons['geometry'].apply(lambda x: self.count_points_in_polygon(x, points['geometry']))
         polygons = polygons.loc[polygons.density > 0]
@@ -54,12 +85,9 @@ class CentralPlace(BaseMethod):
         scaler = MinMaxScaler()
         df_normalized = polygons.copy()
         df_normalized[['median', 'density', 'shannon']] = scaler.fit_transform(polygons[['median', 'density', 'shannon']])
-
         weights = {'median': 1, 'density': 1, 'shannon': 2}
         polygons['centrality'] = (
             weights['density'] * df_normalized['density'] +
             weights['shannon'] * df_normalized['shannon'] -
             weights['median'] * df_normalized['median'])
         return polygons
-        
-        
--- blocksnet/method/vacant_area/vacant_area.py
+++ blocksnet/method/vacant_area/vacant_area.py
@@ -9,6 +9,18 @@ from ..base_method import BaseMethod
 
 
 class VacantArea(BaseMethod):
+    """
+    A class to identify and calculate vacant areas within a specified block or blocks in a city model,
+    taking into account various geographical features like buildings, highways, natural areas, etc.
+
+    Attributes:
+    - local_crs (ClassVar[int]): The local coordinate reference system (CRS) code used for spatial operations.
+    - roads_buffer (ClassVar[int]): The buffer distance to apply around roads when identifying vacant areas.
+    - buildings_buffer (ClassVar[int]): The buffer distance to apply around buildings when identifying vacant areas.
+    - min_length (ClassVar[int]): The minimum length threshold for considered vacant areas.
+    - min_area (ClassVar[int]): The minimum area threshold for considered vacant areas.
+    - area_attitude (ClassVar[int]): The ratio used to filter areas based on their minimum bounding rectangle.
+    """
     local_crs: ClassVar[int] = 32636
     roads_buffer: ClassVar[int] = 10
     buildings_buffer: ClassVar[int] = 10
@@ -19,74 +31,163 @@ class VacantArea(BaseMethod):
 
     @staticmethod
     def _dwn_other(block, local_crs) -> gpd.GeoSeries:
+        """
+        Download other non-standard areas within a block.
+
+        Parameters:
+        - block: The block's polygon geometry.
+        - local_crs: The local coordinate reference system to project the geometries.
+
+        Returns:
+        - A GeoSeries of geometries representing other non-standard areas.
+        """
         try:
             other = ox.features_from_polygon(block, tags={"man_made": True, "aeroway": True, "military": True})
             other["geometry"] = other["geometry"].to_crs(local_crs)
             return other.geometry
-        except:
+        except (ValueError, AttributeError) as exc:
+            print(f"Error encountered: {exc}")
             return gpd.GeoSeries()
 
     @staticmethod
     def _dwn_leisure(block, local_crs) -> gpd.GeoSeries:
+        """
+        Download leisure areas within a block.
+
+        Parameters:
+        - block: The block's polygon geometry.
+        - local_crs: The local coordinate reference system to project the geometries.
+
+        Returns:
+        - A GeoSeries of geometries representing leisure areas.
+        """
         try:
             leisure = ox.features_from_polygon(block, tags={"leisure": True})
             leisure["geometry"] = leisure["geometry"].to_crs(local_crs)
             return leisure.geometry
-        except:
+        except (ValueError, AttributeError) as exc:
+            print(f"Error encountered: {exc}")
             return gpd.GeoSeries()
 
     @staticmethod
     def _dwn_landuse(block, local_crs) -> gpd.GeoSeries:
+        """
+        Download land use areas within a block, excluding residential.
+
+        Parameters:
+        - block: The block's polygon geometry.
+        - local_crs: The local coordinate reference system to project the geometries.
+
+        Returns:
+        - A GeoSeries of geometries representing non-residential land use areas.
+        """
         try:
             landuse = ox.features_from_polygon(block, tags={"landuse": True})
             if not landuse.empty:
                 landuse = landuse[landuse["landuse"] != "residential"]
             landuse["geometry"] = landuse["geometry"].to_crs(local_crs)
             return landuse.geometry
-        except:
+        except (ValueError, AttributeError) as exc:
+            print(f"Error encountered: {exc}")
             return gpd.GeoSeries()
 
     @staticmethod
     def _dwn_amenity(block, local_crs) -> gpd.GeoSeries:
+        """
+        Download amenity areas within a block.
+
+        Parameters:
+        - block: The block's polygon geometry.
+        - local_crs: The local coordinate reference system to project the geometries.
+
+        Returns:
+        - A GeoSeries of geometries representing amenity areas.
+        """
         try:
             amenity = ox.features_from_polygon(block, tags={"amenity": True})
             amenity["geometry"] = amenity["geometry"].to_crs(local_crs)
             return amenity.geometry
-        except:
+        except (ValueError, AttributeError) as exc:
+            print(f"Error encountered: {exc}")
             return gpd.GeoSeries()
 
     @staticmethod
     def _dwn_buildings(block, local_crs, buildings_buffer) -> gpd.GeoSeries:
+        """
+        Download building areas within a block and apply a buffer.
+
+        Parameters:
+        - block: The block's polygon geometry.
+        - local_crs: The local coordinate reference system to project the geometries.
+        - buildings_buffer: Buffer distance to apply to building geometries.
+
+        Returns:
+        - A GeoSeries of buffered building geometries.
+        """
         try:
             buildings = ox.features_from_polygon(block, tags={"building": True})
             if buildings_buffer:
                 buildings["geometry"] = buildings["geometry"].to_crs(local_crs).buffer(buildings_buffer)
             return buildings.geometry
-        except:
+        except (ValueError, AttributeError) as exc:
+            print(f"Error encountered: {exc}")
             return gpd.GeoSeries()
 
     @staticmethod
     def _dwn_natural(block, local_crs) -> gpd.GeoSeries:
+        """
+        Download natural feature areas within a block, excluding bays.
+
+        Parameters:
+        - block: The block's polygon geometry.
+        - local_crs: The local coordinate reference system to project the geometries.
+
+        Returns:
+        - A GeoSeries of geometries representing natural feature areas.
+        """
         try:
             natural = ox.features_from_polygon(block, tags={"natural": True})
             if not natural.empty:
                 natural = natural[natural["natural"] != "bay"]
             natural["geometry"] = natural["geometry"].to_crs(local_crs)
             return natural.geometry
-        except:
+        except (ValueError, AttributeError) as exc:
+            print(f"Error encountered: {exc}")
             return gpd.GeoSeries()
 
     @staticmethod
     def _dwn_waterway(block, local_crs) -> gpd.GeoSeries:
+        """
+        Download waterway areas within a block.
+
+        Parameters:
+        - block: The block's polygon geometry.
+        - local_crs: The local coordinate reference system to project the geometries.
+
+        Returns:
+        - A GeoSeries of geometries representing waterway areas.
+        """
         try:
             waterway = ox.features_from_polygon(block, tags={"waterway": True})
             waterway["geometry"] = waterway["geometry"].to_crs(local_crs)
             return waterway.geometry
-        except:
+        except (ValueError, AttributeError) as exc:
+            print(f"Error encountered: {exc}")
             return gpd.GeoSeries()
 
     @staticmethod
     def _dwn_highway(block, local_crs, roads_buffer) -> gpd.GeoSeries:
+        """
+        Download highway areas within a block, excluding paths, footways, and pedestrian areas, and apply a buffer.
+
+        Parameters:
+        - block: The block's polygon geometry.
+        - local_crs: The local coordinate reference system to project the geometries.
+        - roads_buffer: Buffer distance to apply to highway geometries.
+
+        Returns:
+        - A GeoSeries of buffered highway geometries.
+        """
         try:
             highway = ox.features_from_polygon(block, tags={"highway": True})
             condition = (
@@ -97,44 +198,90 @@ class VacantArea(BaseMethod):
             filtered_highway = highway[condition]
             if roads_buffer:
                 filtered_highway["geometry"] = filtered_highway["geometry"].to_crs(local_crs).buffer(roads_buffer)
-            filtered_highway["geometry"] = filtered_highway["geometry"].to_crs(local_crs).buffer(1)
             return filtered_highway.geometry
-        except:
+        except (ValueError, AttributeError) as exc:
+            print(f"Error encountered: {exc}")
             return gpd.GeoSeries()
 
     @staticmethod
     def _dwn_path(block, local_crs) -> gpd.GeoSeries:
+        """
+        Download path and footway areas within a block and apply a buffer.
+
+        Parameters:
+        - block: The block's polygon geometry.
+        - local_crs: The local coordinate reference system to project the geometries.
+
+        Returns:
+        - A GeoSeries of buffered path and footway geometries.
+        """
         try:
             tags = {"highway": "path", "highway": "footway"}
             path = ox.features_from_polygon(block, tags=tags)
             path["geometry"] = path["geometry"].to_crs(local_crs).buffer(1)
             return path.geometry
-        except:
+        except (ValueError, AttributeError) as exc:
+            print(f"Error encountered: {exc}")
             return gpd.GeoSeries()
 
     @staticmethod
     def _dwn_railway(block, local_crs) -> gpd.GeoSeries:
+        """
+        Download railway areas within a block, excluding subways.
+
+        Parameters:
+        - block: The block's polygon geometry.
+        - local_crs: The local coordinate reference system to project the geometries.
+
+        Returns:
+        - A GeoSeries of geometries representing railway areas.
+        """
         try:
             railway = ox.features_from_polygon(block, tags={"railway": True})
             if not railway.empty:
                 railway = railway[railway["railway"] != "subway"]
             railway["geometry"] = railway["geometry"].to_crs(local_crs)
             return railway.geometry
-        except:
+        except (ValueError, AttributeError) as exc:
+            print(f"Error encountered: {exc}")
             return gpd.GeoSeries()
 
     @staticmethod
     def _create_minimum_bounding_rectangle(polygon) -> gpd.GeoSeries:
+        """
+        Create a minimum bounding rectangle for a given polygon.
+
+        Parameters:
+        - polygon: A polygon geometry for which to create the bounding rectangle.
+
+        Returns:
+        - A GeoSeries containing the minimum bounding rectangle of the input polygon.
+        """
         return polygon.minimum_rotated_rectangle
 
     @staticmethod
     def _buffer_and_union(row, buffer_distance=1.1) -> gpd.GeoSeries:
+        """
+        Apply a buffer to a geometry and return the buffered geometry.
+
+        Parameters:
+        - row: A DataFrame row containing a geometry.
+        - buffer_distance: The distance by which to buffer the geometry.
+
+        Returns:
+        - A GeoSeries containing the buffered geometry.
+        """
         polygon = row["geometry"]
         buffer_polygon = polygon.buffer(buffer_distance)
         return buffer_polygon
 
     def _get_blocks_gdf(self) -> gpd.GeoDataFrame:
-        """Returns blocks gdf for provision assessment"""
+        """
+        Retrieve a GeoDataFrame of blocks for analysis.
+
+        Returns:
+        - A GeoDataFrame containing block geometries and their identifiers.
+        """
         data: list[dict] = []
         for block in self.city_model.blocks:
             data.append({"id": block.id, "geometry": block.geometry})
@@ -142,6 +289,15 @@ class VacantArea(BaseMethod):
         return gdf
 
     def get_vacant_area(self, block: int | Block) -> gpd.GeoDataFrame:
+        """
+        Calculate the vacant area within a given block or blocks.
+
+        Parameters:
+        - block: Either a single block identifier or a Block object representing the area to analyze.
+
+        Returns:
+        - A GeoDataFrame containing the vacant areas within the specified block(s), along with their area and length.
+        """
         blocks = self._get_blocks_gdf()
         blocks = gpd.GeoDataFrame(geometry=gpd.GeoSeries(blocks.geometry))
         if block:
