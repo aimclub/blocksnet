200912606e274f7664ffcc10de845cd830c6d5aa
vasilstar <vasilstar97@gmail.com>
2024-07-24 15:02:57 +0300

    fix: blocks test

--- tests/test_blocks.py
+++ tests/test_blocks.py
@@ -3,38 +3,72 @@
 import os
 import pytest
 import geopandas as gpd
-from blocksnet import BlocksGenerator
+from blocksnet import BlocksGenerator, LandUse, LandUseProcessor
 
-data_path = "./tests/data/blocks"
-local_crs = 32637
+osm_data_path = "./tests/data"
 
-
-@pytest.fixture
-def territory_geometry():
-    return gpd.read_file(os.path.join(data_path, "territory.geojson")).to_crs(local_crs)
+zones_to_lu = {"zone_1": LandUse.RECREATION, "zone_2": LandUse.RESIDENTIAL, "zone_3": LandUse.BUSINESS}
 
 
 @pytest.fixture
-def blocks_cutter(territory_geometry):
-    water_geometry = gpd.read_file(os.path.join(data_path, "water.geojson")).to_crs(local_crs)
-    roads_geometry = gpd.read_file(os.path.join(data_path, "roads.geojson")).to_crs(local_crs)
-    railways_geometry = gpd.read_file(os.path.join(data_path, "railways.geojson")).to_crs(local_crs)
-    return BlocksGenerator(
-        territory=territory_geometry, roads=roads_geometry, railways=railways_geometry, water=water_geometry
+def boundaries():
+    gdf = gpd.read_parquet(os.path.join(osm_data_path, "boundaries.parquet"))
+    crs = gdf.estimate_utm_crs()
+    return gdf.to_crs(crs)
+
+
+@pytest.fixture
+def local_crs(boundaries):
+    return boundaries.crs
+
+
+@pytest.fixture
+def blocks_generator(boundaries, local_crs):
+    gdfs = {
+        name: gpd.read_parquet(os.path.join(osm_data_path, f"{name}.parquet")).to_crs(local_crs)
+        for name in ["roads", "railways", "water"]
+    }
+    return BlocksGenerator(boundaries, **gdfs)
+
+
+@pytest.fixture
+def blocks(blocks_generator):
+    return blocks_generator.run()
+
+
+@pytest.fixture
+def land_use_processor(blocks):
+    zones = blocks.copy()
+    zones.geometry = zones.buffer(1000)
+    zones["zone"] = zones.apply(lambda s: f"zone_{(s.name)%3+1}", axis=1)
+    return LandUseProcessor(blocks, zones, zones_to_lu)
+
+
+@pytest.fixture
+def lu_blocks(land_use_processor):
+    return land_use_processor.run()
+
+
+def test_intersection(blocks):
+    """Check if some of blocks intersect others"""
+    sjoin = gpd.sjoin(blocks, blocks, predicate="intersects")
+    sjoin["intersection"] = sjoin.apply(
+        lambda s: s.geometry.intersection(blocks.loc[s.index_right, "geometry"]), axis=1
     )
+    sjoin["intersects_other"] = sjoin.index != sjoin.index_right
+    sjoin["intersection_poly"] = sjoin["intersection"].geom_type.isin(["Polygon", "MultiPolygon"])
+    assert not any(sjoin["intersects_other"] & sjoin["intersection_poly"])
 
 
-@pytest.fixture
-def blocks(blocks_cutter):
-    return blocks_cutter.generate_blocks()
-
-
-# def test_intersection(blocks):
-#     """Check if some of blocks intersect others"""
-#     blocks_intersection = gpd.sjoin(blocks, blocks, how="inner", predicate="intersects")
-#     assert len(blocks_intersection.loc[lambda x: x.index != x["index_right"]]) == 0
-
-
-def test_within(blocks, territory_geometry):
+def test_within(blocks, boundaries):
     """Check if city blocks are inside initial city geometry with buffer"""
-    assert blocks.within(territory_geometry.geometry.unary_union.buffer(1)).all()
+    assert blocks.within(boundaries.buffer(10).unary_union).all()
+
+
+def test_sameness(blocks, lu_blocks):
+    assert all(blocks.index == lu_blocks.index)
+    assert all(blocks.geometry == lu_blocks.geometry)
+
+
+def test_output(lu_blocks):
+    lu_blocks.to_parquet(os.path.join(osm_data_path, "_blocks.parquet"))
