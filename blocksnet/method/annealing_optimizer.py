6790ee82795af0abf046d05e067fe0eeb7f472ae
Vasilii Starikov <vasilstar97@gmail.com>
2024-09-13 16:28:32 +0300

    feat: annealing optimizer docs and example

--- blocksnet/method/annealing_optimizer.py
+++ blocksnet/method/annealing_optimizer.py
@@ -1,7 +1,9 @@
+"""
+Module providing class for simulated annealing optimization of blocks services constrained by land uses.
+"""
 import random
 import math
 import geopandas as gpd
-from loguru import logger
 import pandas as pd
 from tqdm import tqdm
 from typing import Callable
@@ -14,7 +16,36 @@ LIVING_AREA_DEMAND = 20
 
 
 class Variable:
+    """
+    Represents a variable for optimization in a given block with a specific service type and brick.
+
+    Attributes
+    ----------
+    block : Block
+        The block where the service is located.
+    service_type : ServiceType
+        The type of service provided.
+    brick : ServiceBrick
+        The brick representing the service's characteristics.
+    value : int, optional
+        The quantity of the service brick, by default 0.
+    """
+
     def __init__(self, block: Block, service_type: ServiceType, brick: ServiceBrick, value: int = 0):
+        """
+        Initializes a Variable instance.
+
+        Parameters
+        ----------
+        block : Block
+            The block where the service is located.
+        service_type : ServiceType
+            The type of service provided.
+        brick : ServiceBrick
+            The brick representing the service's characteristics.
+        value : int, optional
+            The quantity of the service brick, by default 0.
+        """
         self.block = block
         self.service_type = service_type
         self.brick = brick
@@ -22,13 +53,38 @@ class Variable:
 
     @property
     def capacity(self):
+        """
+        Calculates the total capacity of the service brick in the block.
+
+        Returns
+        -------
+        int
+            The total capacity of the service brick.
+        """
         return self.brick.capacity * self.value
 
     @property
     def area(self):
+        """
+        Calculates the total area occupied by the service brick.
+
+        Returns
+        -------
+        int
+            The total area occupied by the service brick.
+        """
         return self.brick.area * self.value
 
     def to_dict(self):
+        """
+        Converts the Variable instance to a dictionary representation.
+
+        Returns
+        -------
+        dict
+            A dictionary containing the block ID, service type, brick capacity, brick area, whether the brick is integrated,
+            and the value of the brick.
+        """
         return {
             "block_id": self.block.id,
             "service_type": self.service_type.name,
@@ -40,7 +96,36 @@ class Variable:
 
 
 class Indicator:
+    """
+    Represents indicators for a block based on land use and spatial indices.
+
+    Attributes
+    ----------
+    block : Block
+        The block being evaluated.
+    land_use : LandUse
+        The type of land use for the block.
+    fsi : float
+        The Floor Space Index (FSI) of the block.
+    gsi : float
+        The Ground Space Index (GSI) of the block.
+    """
+
     def __init__(self, block: Block, land_use: LandUse, fsi: float, gsi: float):
+        """
+        Initializes an Indicator instance.
+
+        Parameters
+        ----------
+        block : Block
+            The block being evaluated.
+        land_use : LandUse
+            The type of land use for the block.
+        fsi : float
+            The Floor Space Index (FSI) of the block.
+        gsi : float
+            The Ground Space Index (GSI) of the block.
+        """
         self.block = block
         self.land_use = land_use
         self.fsi = fsi
@@ -48,18 +133,50 @@ class Indicator:
 
     @property
     def site_area(self):
+        """
+        Gets the site area of the block.
+
+        Returns
+        -------
+        float
+            The site area of the block.
+        """
         return self.block.site_area
 
     @property
     def footprint_area(self):
+        """
+        Calculates the footprint area of the block based on the GSI.
+
+        Returns
+        -------
+        float
+            The footprint area of the block.
+        """
         return self.site_area * self.gsi
 
     @property
     def build_floor_area(self):
+        """
+        Calculates the buildable floor area of the block based on the FSI.
+
+        Returns
+        -------
+        float
+            The buildable floor area of the block.
+        """
         return self.site_area * self.fsi
 
     @property
     def integrated_area(self):
+        """
+        Determines the integrated area of the block based on land use.
+
+        Returns
+        -------
+        float
+            The integrated area of the block.
+        """
         if self.land_use == LandUse.RESIDENTIAL:
             return self.footprint_area
         else:
@@ -67,10 +184,26 @@ class Indicator:
 
     @property
     def non_integrated_area(self):
+        """
+        Calculates the non-integrated area of the block.
+
+        Returns
+        -------
+        float
+            The non-integrated area of the block.
+        """
         return VACANT_AREA_COEF * self.site_area - self.footprint_area
 
     @property
     def living_area(self):
+        """
+        Determines the living area of the block based on land use.
+
+        Returns
+        -------
+        float
+            The living area of the block. Returns 0 if land use is not residential.
+        """
         if self.land_use == LandUse.RESIDENTIAL:
             return self.build_floor_area - self.integrated_area
         else:
@@ -78,9 +211,26 @@ class Indicator:
 
     @property
     def population(self):
+        """
+        Estimates the population of the block based on living area and demand.
+
+        Returns
+        -------
+        int
+            The estimated population of the block.
+        """
         return math.floor(self.living_area / LIVING_AREA_DEMAND)
 
     def to_dict(self) -> dict:
+        """
+        Converts the Indicator instance to a dictionary representation.
+
+        Returns
+        -------
+        dict
+            A dictionary containing block ID, site area, land use, footprint area, build floor area, integrated area,
+            non-integrated area, living area, and population.
+        """
         res = {
             "block_id": self.block.id,
             "site_area": self.site_area,
@@ -96,11 +246,34 @@ class Indicator:
 
 
 class AnnealingOptimizer(BaseMethod):
+    """
+    Services optimizer using simulated annealing to maximize scenario provision objective function by building certain services.
+
+    Attributes
+    ----------
+    on_iteration : Callable[[int, list[Variable], dict[int, Indicator], float], None] | None
+        A callback function to be called after each iteration with the current iteration number,
+        the current list of variables, indicators, and the best value found so far.
+    """
 
     on_iteration: Callable[[int, list[Variable], dict[int, Indicator], float], None] | None = None
 
-    def _check_constraints(self, X, indicators):
+    def _check_constraints(self, X, indicators) -> bool:
+        """
+        Checks if the current solution satisfies all constraints.
 
+        Parameters
+        ----------
+        X : list[Variable]
+            List of variables representing the current solution.
+        indicators : dict[int, Indicator]
+            Dictionary mapping block IDs to their corresponding indicators.
+
+        Returns
+        -------
+        bool
+            True if all constraints are satisfied, False otherwise.
+        """
         df = self.to_bricks_df(X)
         df["integrated_area"] = df.apply(lambda s: s["area"] * s["count"] if s["is_integrated"] else 0, axis=1)
         df["non_integrated_area"] = df.apply(lambda s: s["area"] * s["count"] if not s["is_integrated"] else 0, axis=1)
@@ -123,7 +296,20 @@ class AnnealingOptimizer(BaseMethod):
         return True
 
     @staticmethod
-    def _perturb(X: list[Variable]):
+    def _perturb(X: list[Variable]) -> tuple[list[Variable], ServiceType]:
+        """
+        Perturbs the current solution by modifying a randomly chosen variable.
+
+        Parameters
+        ----------
+        X : list[Variable]
+            List of variables representing the current solution.
+
+        Returns
+        -------
+        tuple[list[Variable], ServiceType]
+            A tuple containing the perturbed list of variables and the service type of the perturbed variable.
+        """
         new_X = [Variable(x.block, x.service_type, x.brick, x.value) for x in X]
         x = random.choice(new_X)
         delta = random.choice([-1, 1])
@@ -131,6 +317,21 @@ class AnnealingOptimizer(BaseMethod):
         return new_X, x.service_type
 
     def _generate_initial_X(self, blocks_lu: dict[int, LandUse], service_types: dict[str, float]) -> list[Variable]:
+        """
+        Generates an initial list of variables for the optimization.
+
+        Parameters
+        ----------
+        blocks_lu : dict[int, LandUse]
+            Dictionary mapping block IDs to their corresponding land uses.
+        service_types : dict[str, float]
+            Dictionary mapping service type names to their weights.
+
+        Returns
+        -------
+        list[Variable]
+            A list of variables representing the initial solution.
+        """
         X = []
         for block_id, land_use in blocks_lu.items():
             block = self.city_model[block_id]
@@ -143,15 +344,60 @@ class AnnealingOptimizer(BaseMethod):
         return X
 
     def _generate_indicators(self, blocks, fsis, gsis) -> dict[int, Indicator]:
+        """
+        Generates indicators for each block based on the provided data.
+
+        Parameters
+        ----------
+        blocks : dict[int, LandUse]
+            Dictionary mapping block IDs to their corresponding land uses.
+        fsis : dict[int, float]
+            Dictionary mapping block IDs to their Floor Space Index (FSI) values.
+        gsis : dict[int, float]
+            Dictionary mapping block IDs to their Ground Space Index (GSI) values.
+
+        Returns
+        -------
+        dict[int, Indicator]
+            A dictionary mapping block IDs to their corresponding indicators.
+        """
         return {b_id: Indicator(self.city_model[b_id], blocks[b_id], fsis[b_id], gsis[b_id]) for b_id in blocks.keys()}
 
-    def to_gdf(self, X: list[Variable], indicators: dict[int, Indicator]):
+    def to_gdf(self, X: list[Variable], indicators: dict[int, Indicator]) -> gpd.GeoDataFrame:
+        """
+        Converts the list of variables and indicators into a GeoDataFrame.
+
+        Parameters
+        ----------
+        X : list[Variable]
+            List of variables representing the current solution.
+        indicators : dict[int, Indicator]
+            Dictionary mapping block IDs to their corresponding indicators.
+
+        Returns
+        -------
+        gpd.GeoDataFrame
+            A GeoDataFrame containing optimization result data.
+        """
         df = self.to_df(X, indicators)
         df["geometry"] = df.apply(lambda s: self.city_model[s.name].geometry, axis=1)
         df["land_use"] = df.apply(lambda s: indicators[s.name].land_use.value, axis=1)
         return gpd.GeoDataFrame(df, crs=self.city_model.crs)
 
-    def to_bricks_df(self, X: list[Variable]):
+    def to_bricks_df(self, X: list[Variable]) -> pd.DataFrame:
+        """
+        Converts the list of variables into a DataFrame representing the bricks.
+
+        Parameters
+        ----------
+        X : list[Variable]
+            List of variables representing the current solution.
+
+        Returns
+        -------
+        pd.DataFrame
+            A DataFrame containing data about the bricks in the solution.
+        """
         xs = [
             {
                 "block_id": x.block.id,
@@ -163,10 +409,49 @@ class AnnealingOptimizer(BaseMethod):
             }
             for x in X
         ]
-        # xs = filter(lambda v : v['count']>0, xs)
-        return pd.DataFrame(list(xs))
+        df = pd.DataFrame(list(xs))
+        return df[df["count"] != 0]
 
-    def to_df(self, X: list[Variable], indicators: dict[int, Indicator]):
+    def _get_clear_df(self, blocks: list[int]) -> pd.DataFrame:
+        """
+        Constructs a DataFrame for provision assessment so the blocks being changed are treated as cleared.
+
+        Parameters
+        ----------
+        blocks : list[int]
+            List of changing blocks ids.
+
+        Returns
+        -------
+        pd.DataFrame
+            A DataFrame containing information related to blocks being changed for provision assessment.
+        """
+        gdf = self.city_model.get_blocks_gdf()
+        gdf = gdf[gdf.index.isin(blocks)]
+        df = gdf[["population"]].copy()
+        df["population"] = -df["population"]
+        df.sum()
+        for column in [column for column in gdf.columns if "capacity_" in column]:
+            st_name = column.removeprefix("capacity_")
+            df[st_name] = -gdf[column]
+        return df
+
+    def to_df(self, X: list[Variable], indicators: dict[int, Indicator]) -> pd.DataFrame:
+        """
+        Converts the list of variables and indicators into a DataFrame.
+
+        Parameters
+        ----------
+        X : list[Variable]
+            List of variables representing the current solution.
+        indicators : dict[int, Indicator]
+            Dictionary mapping block IDs to their corresponding indicators.
+
+        Returns
+        -------
+        pd.DataFrame
+            A DataFrame containing data about the blocks and their service capacities.
+        """
         service_types = {x.service_type for x in X}
         df = pd.DataFrame(
             [
@@ -191,19 +476,47 @@ class AnnealingOptimizer(BaseMethod):
         rate: float = 0.95,
         max_iter: int = 1000,
     ) -> tuple:
+        """
+        Executes the services optimization process based on simulated annealing for provided service types weights and blocks landuses.
 
+        Parameters
+        ----------
+        blocks_lu : dict[int, LandUse]
+            Dictionary mapping block IDs to their corresponding land uses.
+        blocks_fsi : dict[int, float]
+            Dictionary mapping block IDs to their Floor Space Index (FSI) values.
+        blocks_gsi : dict[int, float]
+            Dictionary mapping block IDs to their Ground Space Index (GSI) values.
+        service_types : dict[str, float]
+            Dictionary mapping service type names to their weights.
+        t_max : float, optional
+            The maximum temperature for the annealing process, by default 100.
+        t_min : float, optional
+            The minimum temperature for the annealing process, by default 1e-3.
+        rate : float, optional
+            The cooling rate of the temperature, by default 0.95.
+        max_iter : int, optional
+            The maximum number of iterations, by default 1000.
+
+        Returns
+        -------
+        tuple
+            A tuple containing the best solution found, the indicators, the best value, and the provision values for each service type.
+        """
         indicators = self._generate_indicators(blocks_lu, blocks_fsi, blocks_gsi)
 
         best_X = self._generate_initial_X(blocks_lu, service_types)
         best_value = 0
 
+        clear_df = self._get_clear_df(blocks_lu.keys())
+
         prov = Provision(city_model=self.city_model, verbose=False)
 
         def calculate_provision(X, service_type):
-            df = self.to_df(X, indicators)
-            if df[service_type.name].sum() == 0:
+            update_df = clear_df.add(self.to_df(X, indicators))
+            if update_df[service_type.name].sum() == 0:
                 return 0
-            gdf = prov.calculate(service_type, df, self_supply=True)
+            gdf = prov.calculate(service_type, update_df, self_supply=True)
             return prov.total(gdf)
 
         provisions = {st: 0.0 for st in service_types.keys()}
--- blocksnet/method/land_use_optimizer.py
+++ blocksnet/method/land_use_optimizer.py
@@ -1,11 +1,47 @@
+import statistics
 import pygad
 import geopandas as gpd
+import networkx as nx
 from loguru import logger
 from tqdm import tqdm
 from pydantic import Field
 from .base_method import BaseMethod
 from ..models import Block, LandUse
 from .annealing_optimizer import AnnealingOptimizer
+
+ADJACENCY_RULES = [
+    # self adjacency
+    (LandUse.RESIDENTIAL, LandUse.RESIDENTIAL),
+    (LandUse.BUSINESS, LandUse.BUSINESS),
+    (LandUse.RECREATION, LandUse.RECREATION),
+    (LandUse.INDUSTRIAL, LandUse.INDUSTRIAL),
+    (LandUse.TRANSPORT, LandUse.TRANSPORT),
+    (LandUse.SPECIAL, LandUse.SPECIAL),
+    (LandUse.AGRICULTURE, LandUse.AGRICULTURE),
+    # recreation can be adjacent to anything
+    (LandUse.RECREATION, LandUse.SPECIAL),
+    (LandUse.RECREATION, LandUse.INDUSTRIAL),
+    (LandUse.RECREATION, LandUse.BUSINESS),
+    (LandUse.RECREATION, LandUse.AGRICULTURE),
+    (LandUse.RECREATION, LandUse.TRANSPORT),
+    (LandUse.RECREATION, LandUse.RESIDENTIAL),
+    # residential
+    (LandUse.RESIDENTIAL, LandUse.BUSINESS),
+    # business
+    (LandUse.BUSINESS, LandUse.INDUSTRIAL),
+    (LandUse.BUSINESS, LandUse.TRANSPORT),
+    # industrial
+    (LandUse.INDUSTRIAL, LandUse.SPECIAL),
+    (LandUse.INDUSTRIAL, LandUse.AGRICULTURE),
+    (LandUse.INDUSTRIAL, LandUse.TRANSPORT),
+    # transport
+    (LandUse.TRANSPORT, LandUse.SPECIAL),
+    (LandUse.TRANSPORT, LandUse.AGRICULTURE),
+    # special
+    (LandUse.SPECIAL, LandUse.AGRICULTURE),
+]
+
+RULES_GRAPH = nx.from_edgelist(ADJACENCY_RULES)
 
 
 class Indicator:
@@ -52,22 +88,37 @@ class LandUseOptimizer(BaseMethod):
     def annealing_optimizer(self):
         return AnnealingOptimizer(city_model=self.city_model, verbose=False)
 
-    @log_execution_time
-    def _fitness(self, blocks_lu: dict[int, LandUse], service_types: dict[str, float], objective: LandUse):
+    def _fitness_by_share(self, blocks_lu: dict[int, LandUse], lu_shares: dict[LandUse, float]) -> float:
+        gdf = gpd.GeoDataFrame(
+            [{"id": i, "geometry": self.city_model[i].geometry, "land_use": lu} for i, lu in blocks_lu.items()],
+            crs=self.city_model.crs,
+        ).set_index("id", drop=True)
+        area = gdf.area.sum()
+        shares = []
+        for lu, share in lu_shares.items():
+            lu_area = gdf[gdf.land_use == lu].area.sum()
+            lu_share = lu_area / area
+            shares.append(abs(lu_share - share))
+        return statistics.mean(shares)
+
+    def _fitness_by_rules(self, blocks_lu, blocks_graph) -> float:
+        all_edges = blocks_graph.edges
+        proper_edges = [(u, v) for u, v in blocks_graph.edges if RULES_GRAPH.has_edge(blocks_lu[u], blocks_lu[v])]
+        return len(proper_edges) / len(all_edges)
+
+    def _fitness_by_provision(self, blocks_lu: dict[int, LandUse], service_types: dict[str, float]) -> float:
+
+        if not LandUse.RESIDENTIAL in blocks_lu.keys():
+            return 0
 
         ao = self.annealing_optimizer
 
         blocks_fsi = {b_id: LU_INDICATORS[lu].fsi_min for b_id, lu in blocks_lu.items()}
         blocks_gsi = {b_id: LU_INDICATORS[lu].gsi_min for b_id, lu in blocks_lu.items()}
-        X, indicators, value, _ = ao.calculate(
+        _, _, value, _ = ao.calculate(
             blocks_lu, blocks_fsi, blocks_gsi, service_types, rate=self.annealing_rate, max_iter=self.max_iter
         )
-
-        gdf = ao.to_gdf(X, indicators)
-        area = gdf.area.sum()
-        lu_area = gdf[gdf.land_use == objective.value].area.sum()
-
-        return value, lu_area / area
+        return value
 
     def to_gdf(self, blocks: dict[int, LandUse]):
         return gpd.GeoDataFrame(
@@ -78,16 +129,28 @@ class LandUseOptimizer(BaseMethod):
             crs=self.city_model.crs,
         ).set_index("block_id", drop=True)
 
+    @staticmethod
+    def _get_blocks_adjacency_graph(blocks: list[Block], buffer=1):
+        crs = blocks[0].city.crs
+        gdf = gpd.GeoDataFrame(
+            [{"id": block.id, "geometry": block.geometry.buffer(buffer)} for block in blocks], crs=crs
+        ).set_index("id", drop=True)
+        sjoin = gpd.sjoin(gdf[["geometry"]], gdf[["geometry"]], predicate="intersects")
+        edge_list = [(i, s.index_right) for i, s in sjoin[sjoin.index != sjoin.index_right][["index_right"]].iterrows()]
+        return nx.from_edgelist(edge_list)
+
     def calculate(
         self,
         blocks: list[Block],
         service_types: dict[str, float],
-        objective: LandUse,
+        lu_shares: dict[LandUse, float],
         generations: int = 10,
         parents: int = 4,
         solutions: int = 8,
         # processes : int = 8
     ) -> tuple[dict[int, LandUse], tuple[float, float], pygad.GA]:
+
+        blocks_graph = self._get_blocks_adjacency_graph(blocks)
 
         lu_mapping = {i: lu for i, lu in enumerate(LandUse)}
         gene_space = [[v for v in lu_mapping.keys()] for _ in blocks]
@@ -95,25 +158,33 @@ class LandUseOptimizer(BaseMethod):
         def fitness_func(ga_instance, solution, solution_idx):
 
             blocks_lu = {blocks[i].id: lu_mapping[lu_i] for i, lu_i in enumerate(solution)}
-            fitness = self._fitness(blocks_lu, service_types, objective)
-            return fitness
+            rules_ratio = self._fitness_by_rules(blocks_lu, blocks_graph)
+            share_value = self._fitness_by_share(blocks_lu, lu_shares)
+            provision_value = self._fitness_by_provision(blocks_lu, service_types)
+            return rules_ratio + rules_ratio * share_value + rules_ratio * share_value * provision_value
+
+        if self.verbose:
+            pbar = tqdm(range(0, generations))
 
         def on_generation(ga_instance):
             if self.verbose:
-                logger.info(f"Best fitness : {[round(v,2) for v in ga_instance.best_solution()[1]]}")
+                pbar.update(1)
+                pbar.set_description_str(f"{round(ga_instance.best_solution()[1],2)}")
 
         ga_instance = pygad.GA(
             fitness_func=fitness_func,
             on_generation=on_generation,
             num_generations=generations,
             num_parents_mating=parents,
-            parent_selection_type="tournament",
-            crossover_type="single_point",
+            parent_selection_type="rank",
+            crossover_type="scattered",
             sol_per_pop=solutions,
             # parallel_processing=processes,
             num_genes=len(gene_space),
             gene_space=gene_space,
-            keep_elitism=2,
+            keep_elitism=parents,
+            # mutation_probability=0.01,
+            # stop_criteria='reach_1'
         )
         ga_instance.run()
 
