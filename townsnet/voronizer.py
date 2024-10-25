bb334554abc261a17a77fd182c93e0465d7ed6ad
vasilstar <vasilstar97@gmail.com>
2024-10-25 18:48:49 +0300

    fix: voronizer based on geopandas >= 1.0.0

--- townsnet/voronizer.py
+++ townsnet/voronizer.py
@@ -2,7 +2,6 @@ import pandera as pa
 import pandas as pd
 import geopandas as gpd
 import shapely
-from longsgis import voronoiDiagram4plg # pip install voronoi-diagram-for-polygons
 from tqdm import tqdm
 from .base_schema import BaseSchema
 
@@ -16,7 +15,7 @@ class SettlementsSchema(BaseSchema):
     return series.representative_point().rename(name)
 
 class UnitsSchema(BaseSchema):
-  _geom_types = [shapely.Polygon]
+  _geom_types = [shapely.Polygon, shapely.MultiPolygon]
 
 class Voronizer():
 
@@ -25,15 +24,8 @@ class Voronizer():
     self.local_crs = settlements_gdf.estimate_utm_crs()
 
     self.settlements_gdf = SettlementsSchema(settlements_gdf.to_crs(self.local_crs))
-    units_gdf = units_gdf.explode(index_parts = True).reset_index().copy()
+    # units_gdf = units_gdf.explode(index_parts = True).reset_index().copy()
     self.units_gdf = UnitsSchema(units_gdf.to_crs(self.local_crs))
-
-  @staticmethod
-  def _voronize(points_gdf : gpd.GeoDataFrame, polygon_gdf : gpd.GeoDataFrame) -> gpd.GeoDataFrame:
-    if len(points_gdf) == 1:
-      return polygon_gdf.copy()
-    vd = voronoiDiagram4plg(points_gdf[['geometry']], polygon_gdf[['geometry']])
-    return vd[['geometry']]
 
   def run(self):
     #polygonize every unit settlements
@@ -42,11 +34,12 @@ class Voronizer():
     sjoin = settlements_gdf.sjoin(units_gdf, how='left', predicate='within')
     voronoi_gdfs = []
     for unit_i, unit_settlements_gdf in tqdm(sjoin.groupby('index_right')):
-      unit_gdf = units_gdf[units_gdf.index == unit_i]
-      voronoi_gdf = self._voronize(unit_settlements_gdf, unit_gdf)
+      unit_polygon = units_gdf.loc[unit_i].geometry
+      voronoi_geoseries = unit_settlements_gdf.geometry.voronoi_polygons(extend_to=unit_polygon)
+      voronoi_gdf = gpd.GeoDataFrame(geometry=voronoi_geoseries.clip(unit_polygon))
       voronoi_gdfs.append(voronoi_gdf)
     #concat resulting gdf
     gdf = pd.concat(voronoi_gdfs).reset_index(drop=True)[['geometry']]
     sjoin = settlements_gdf.sjoin(gdf, predicate='within')
     sjoin.geometry = sjoin['index_right'].apply(lambda ir : gdf.loc[ir].geometry)
-    return sjoin[['geometry']].to_crs(self.global_crs)
+    return gdf
