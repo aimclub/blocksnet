5891ca25cce059c4f93d3db2590f690260b9dc4e
MarkBzm <mark472013316@gmail.com>
2025-02-02 06:27:33 +0300

    arch(acl)

---
+++ blocksnet/optimizer/src/acl/__init__.py
@@ -0,0 +1,4 @@
+from .area_checker import AreaChecker
+from .facade import BlocksnetFacade
+from .provision_adapter import ProvisionAdapter
+from .variable_adapter import BlockSolution, ServiceSolution, VariableAdapter
--- blocksnet/optimizer/src/acl/facade.py
+++ blocksnet/optimizer/src/acl/facade.py
@@ -1,16 +1,44 @@
+import copy
 from typing import Dict
 
-from blocksnet.method.annealing_optimizer import Indicator, LandUse, Variable
+import numpy as np
+from numpy.typing import ArrayLike
+
+from blocksnet.method.annealing_optimizer import Indicator, LandUse
 from blocksnet.models import City
 
+from .area_checker import AreaChecker
 from .provision_adapter import ProvisionAdapter
 from .variable_adapter import VariableAdapter
-from .constraints_checker import Constraints
 
 
 class BlocksnetFacade:
     """
-    Facade class for managing domain-related operations, including provision calculations.
+    The BlocksnetFacade class serves as a high-level interface for managing various 
+    domain-related operations in an urban planning context. It integrates 
+    variable adaptation, provision calculations, and area validation.
+
+    Responsibilities
+    ----------------
+    - Manages land use data and associated indicators.
+    - Provides an interface for computing urban planning metrics.
+    - Handles transformations between raw input variables and domain-specific representations.
+    - Computes distances for urban layout optimization.
+
+    Attributes
+    ----------
+    _city : City
+        The city model representing the urban layout.
+    _blocks_lu : Dict[int, LandUse]
+        A dictionary mapping block IDs to their respective land use categories.
+    _area_checker : AreaChecker
+        Utility for evaluating constraints and compliance with area-based conditions.
+    _provision_adapter : ProvisionAdapter
+        Adapter responsible for recalculating service provisions based on solution vector.
+    _converter : VariableAdapter
+        Adapter for converting optimization variables into domain-specific structures.
+    _indicators : Dict[int, Indicator]
+        A mapping of block IDs to their respective indicators, containing urban planning metrics.
     """
 
     def __init__(
@@ -33,22 +61,15 @@ class BlocksnetFacade:
         blocks_gsi : Dict[int, float]
             Dictionary mapping block IDs to their Ground Space Index (GSI) values.
         city_model : City
-            The city model object representing the urban layout.
+            The city model object represents a block-network city information model.
         variable_adapter : VariableAdapter
             Adapter for converting solution variables into domain-specific data.
         """
-        self.city: City = city_model
-        self.blocks_lu: Dict[int, LandUse] = blocks_lu
-        self.provision_adapter: ProvisionAdapter = ProvisionAdapter(city_model, blocks_lu, blocks_gsi)
-        self.constraints_checker: Constraints = #TODO
-        self.converter: VariableAdapter = variable_adapter
+        pass
 
-        # Generate indicators for the blocks
-        self.indicators: Dict[int, Indicator] = self._generate_indicators(
-            blocks_lu, blocks_fsi, blocks_gsi
-        )
-
-    def _generate_indicators(self, blocks: Dict[int, LandUse], fsis: Dict[int, float], gsis: Dict[int, float]) -> Dict[int, Indicator]:
+    def _generate_indicators(
+        self, blocks: Dict[int, LandUse], fsis: Dict[int, float], gsis: Dict[int, float]
+    ) -> Dict[int, Indicator]:
         """
         Generates indicators for each block based on the provided data.
 
@@ -66,8 +87,63 @@ class BlocksnetFacade:
         dict[int, Indicator]
             A dictionary mapping block IDs to their corresponding indicators.
         """
-        return {
-            b_id: Indicator(self.city[b_id], blocks[b_id], fsis[b_id], gsis[b_id])
-            for b_id in blocks.keys()
-        }
-    pass
+        return {b_id: Indicator(self._city[b_id], blocks[b_id], fsis[b_id], gsis[b_id]) for b_id in blocks.keys()}
+
+    def get_distance(self, x: ArrayLike) -> float:
+        """
+        Calculates the distance for the solution.
+
+        Parameters
+        ----------
+        x : ArrayLike
+            The input values representing the state of the blocks.
+
+        Returns
+        -------
+        float
+            The calculated distance.
+        """
+        X = self._converter(x)
+        return self._area_checker.get_distance_for_entire_solution(X)
+
+    def get_max_distance(self) -> float:
+        """
+        Retrieves the maximum possible distance for the current block setup.
+
+        Returns
+        -------
+        float
+            The maximum distance.
+        """
+        pass
+
+    def get_provisions(self, x: ArrayLike) -> Dict[str, float]:
+        pass
+
+    def get_changed_services_count(self, x: ArrayLike) -> int:
+       pass
+
+    def get_upper_bound_var(self, var_num: int) -> int:
+        """
+        Retrieves the upper bound for the variable at the given position.
+
+        Parameters
+        ----------
+        var_pos : int
+            The variable position.
+
+        Returns
+        -------
+        int
+            The upper bound for the variable.
+        """
+        pass
+
+    def get_var_weight(self, var_num: int) -> int:
+        pass
+
+    def get_limit(self, block_id: int) -> float:
+        pass
+
+    def get_group_num(self, var_num: int) -> int:
+        pass
