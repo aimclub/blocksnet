38dd18aafa9727dc041bf562d44410c9a03058d8
Vasilii Starikov <vasilstar97@gmail.com>
2025-02-06 15:56:57 +0300

    fix: version changed

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -25,7 +25,7 @@ copyright = "2023-{}, IDU".format(datetime.datetime.now().year)
 author = "IDU"
 
 # The full version, including alpha/beta/rc tags
-release = "0.0.12"
+release = "0.1.0"
 # -- General configuration ---------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
