dac8e1e0bbd65be2d9cd7042298b1f9821ecdb45
vasilstar <vasilstar97@gmail.com>
2025-03-26 16:35:26 +0300

    fix: aggregate only centroids

--- blocksnet/blocks/aggregation/core.py
+++ blocksnet/blocks/aggregation/core.py
@@ -3,52 +3,27 @@ import pandas as pd
 from loguru import logger
 from tqdm import tqdm
 from .schemas import BlocksSchema
-from ...config import log_config
 from ...utils.validation import ensure_crs
 
-COUNT_COLUMN = "count"
-OBJECT_INDEX_COLUMN = "_object_index"
-BLOCK_ID_COLUMN = "_block_id"
-INTERSECTION_AREA_COLUMN = "_intersection_area"
+OBJECTS_COUNT_COLUMN = "objects_count"
 
 
-def _validate_input(blocks_gdf: gpd.GeoDataFrame, objects_gdf: gpd.GeoDataFrame):
-    logger.info("Validating input.")
-    if not isinstance(objects_gdf, gpd.GeoDataFrame):
-        raise ValueError("objects_gdf must be an instance of GeoDataFrame.")
+def _preprocess_input(blocks_gdf: gpd.GeoDataFrame, objects_gdf: gpd.GeoDataFrame):
+    logger.info("Preprocessing input")
     ensure_crs(blocks_gdf, objects_gdf)
-    if COUNT_COLUMN in objects_gdf.columns:
+    objects_gdf["geometry"] = objects_gdf.representative_point()
+    if OBJECTS_COUNT_COLUMN in objects_gdf.columns:
         logger.warning(
-            f"Column {COUNT_COLUMN} found in objects_gdf. It will be taken into account and might affect the result."
+            f"Column {OBJECTS_COUNT_COLUMN} found in objects_gdf. It will be taken into account and might affect the result."
         )
     else:
-        objects_gdf[COUNT_COLUMN] = 1
+        objects_gdf[OBJECTS_COUNT_COLUMN] = 1
 
 
-def _intersect_objects_with_blocks(objects_gdf: gpd.GeoDataFrame, blocks_gdf: gpd.GeoDataFrame):
-    objects_gdf = objects_gdf.copy()
-    intersections_gdf = gpd.overlay(objects_gdf, blocks_gdf, how="intersection")
-    intersections_gdf[INTERSECTION_AREA_COLUMN] = intersections_gdf.geometry.area
-    return intersections_gdf
-
-
-def _keep_largest_intersections(intersections_gdf: gpd.GeoDataFrame) -> gpd.GeoDataFrame:
-    intersections_gdf = intersections_gdf.copy()
-    idx = intersections_gdf.groupby(OBJECT_INDEX_COLUMN)[INTERSECTION_AREA_COLUMN].idxmax()
-    return intersections_gdf.loc[idx]
-
-
-def _label_intersections_with_blocks(intersections_gdf: gpd.GeoDataFrame, blocks_gdf: gpd.GeoDataFrame) -> pd.DataFrame:
-    intersections_gdf = intersections_gdf.copy()
-    intersections_gdf.geometry = intersections_gdf.representative_point()
-    intersections_gdf = gpd.sjoin(intersections_gdf, blocks_gdf, predicate="intersects")
-    return intersections_gdf.drop(columns=["geometry"]).rename({"index_right": "_block_id"}, axis=1)
-
-
-def _get_agg_dict(objects_gdf: gpd.GeoDataFrame):
+def _get_agg_rules(objects_gdf: gpd.GeoDataFrame):
     agg_dict = {}
     for col in objects_gdf.columns:
-        if col == "geometry" or col in [OBJECT_INDEX_COLUMN, INTERSECTION_AREA_COLUMN, BLOCK_ID_COLUMN]:
+        if col == "geometry":
             continue
         dtype = objects_gdf[col].dtype
         if pd.api.types.is_numeric_dtype(dtype):
@@ -56,40 +31,19 @@ def _get_agg_dict(objects_gdf: gpd.GeoDataFrame):
     return agg_dict
 
 
-def _aggregate_objects(
-    blocks_gdf: gpd.GeoDataFrame,
-    objects_gdf: gpd.GeoDataFrame,
-):
-    blocks_gdf = blocks_gdf.copy()
-    objects_gdf = objects_gdf.copy()
-    objects_gdf[OBJECT_INDEX_COLUMN] = objects_gdf.index
-    intersections_gdf = _intersect_objects_with_blocks(objects_gdf, blocks_gdf)
-    intersections_gdf = _keep_largest_intersections(intersections_gdf)
-    intersections_gdf = _label_intersections_with_blocks(intersections_gdf, blocks_gdf)
-
-    intersections_groupby = intersections_gdf.groupby(BLOCK_ID_COLUMN)
-    aggregated_blocks_gdf = intersections_groupby.agg(_get_agg_dict(objects_gdf))
-
-    for col in aggregated_blocks_gdf.columns:
-        if not col in blocks_gdf.columns:
-            blocks_gdf[col] = 0
-        blocks_gdf[col] = blocks_gdf[col].add(aggregated_blocks_gdf[col], fill_value=0)
-
-    return blocks_gdf
-
-
-def aggregate_objects(blocks_gdf: gpd.GeoDataFrame, objects_gdf: gpd.GeoDataFrame):
+def aggregate_objects(
+    blocks_gdf: gpd.GeoDataFrame, objects_gdf: gpd.GeoDataFrame
+) -> tuple[gpd.GeoDataFrame, gpd.GeoDataFrame]:
     blocks_gdf = BlocksSchema(blocks_gdf)
     objects_gdf = objects_gdf.copy()
+    _preprocess_input(blocks_gdf, objects_gdf)
 
-    _validate_input(blocks_gdf, objects_gdf)
+    logger.info("Aggregating objects")
+    agg_rules = _get_agg_rules(objects_gdf)
+    sjoin = objects_gdf.sjoin(blocks_gdf)
+    agg = sjoin.groupby("index_right").agg(agg_rules)
+    blocks_gdf = blocks_gdf.join(agg).fillna(0)
 
-    # blocks_gdf = _initialize_columns(blocks_gdf, objects_gdf)
+    excess_objects_gdf = objects_gdf[~objects_gdf.index.isin(sjoin.index)].copy()
 
-    logger.info("Aggregating objects.")
-    geom_types = objects_gdf.geom_type.unique()
-    for geom_type in tqdm(geom_types, disable=log_config.disable_tqdm):
-        objects_gdf = objects_gdf[objects_gdf.geom_type == geom_type]
-        blocks_gdf = _aggregate_objects(blocks_gdf, objects_gdf)
-
-    return blocks_gdf
+    return blocks_gdf, excess_objects_gdf
--- blocksnet/blocks/aggregation/schemas.py
+++ blocksnet/blocks/aggregation/schemas.py
@@ -1,16 +1,10 @@
 import shapely
 from pandera.typing import Series
-from pandera import Field
+from pandera import Field, parser
 from ...utils.validation import GdfSchema
 
 
 class BlocksSchema(GdfSchema):
-    @classmethod
-    def _geometry_types(cls):
-        return {shapely.Polygon}
-
-
-class ObjectsSchema(GdfSchema):
     @classmethod
     def _geometry_types(cls):
         return {shapely.Polygon}
