394b74df69f9b5f0d8a36be258908cdbe995cf14
vasilstar <vasilstar97@gmail.com>
2025-04-09 17:38:11 +0300

    feat: land use shares indicators calculator

--- blocksnet/analysis/land_use/share/core.py
+++ blocksnet/analysis/land_use/share/core.py
@@ -3,7 +3,7 @@ from .schemas import BlocksSchema
 from ....enums.land_use import LandUse
 
 
-def land_use_shares(blocks_df: pd.DataFrame, area: float | None = None) -> dict[LandUse, float]:
+def calculate_land_use_shares(blocks_df: pd.DataFrame, area: float | None = None) -> dict[LandUse, float]:
     blocks_df = BlocksSchema(blocks_df)
 
     if area is None:
