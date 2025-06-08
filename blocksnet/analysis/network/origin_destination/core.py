fix: removed hebo from requirements fix: removed nodes_gdf from od_matrix calculation

blocksnet/analysis/network/origin_destination/core.py
@@ -4,10 +4,8 @@ import numpy as np
 from sklearn.preprocessing import MinMaxScaler
 from loguru import logger
 from blocksnet.enums import LandUse
-from blocksnet.relations import validate_accessibility_matrix
 from blocksnet.analysis.provision.diversity.core import shannon_diversity, SHANNON_DIVERSITY_COLUMN, COUNT_COLUMN
-from blocksnet.utils.validation import ensure_crs
-from .schemas import BlocksSchema, NodesSchema
+from .schemas import BlocksSchema
 
 DENSITY_COLUMN = "density"
 LU_CONST_COLUMN = "lu_const"
@@ -27,9 +25,7 @@ DEFAULT_LU_CONST = 0.06
 DEFAULT_ACCESSIBILITY = 10
 
 
-def _calculate_nodes_weights(
-    blocks_df: gpd.GeoDataFrame, nodes_gdf: gpd.GeoDataFrame, acc_mx: pd.DataFrame, accessibility: float
-) -> None:
+def _calculate_nodes_weights(blocks_df: gpd.GeoDataFrame, acc_mx: pd.DataFrame, accessibility: float) -> pd.DataFrame:
 
     logger.info("Identifying nearest nodes to blocks")
     acc_mx = acc_mx.replace(0, 0.1)
@@ -43,9 +39,10 @@ def _calculate_nodes_weights(
     weights_mx = weights_mx.div(weights_sum, axis=0)
 
     logger.info("Distributing")
-    nodes_gdf[ATTRACTIVENESS_COLUMN] = weights_mx.mul(blocks_df[ATTRACTIVENESS_COLUMN], axis=0).sum(axis=0)
-    nodes_gdf[POPULATION_COLUMN] = weights_mx.mul(blocks_df[POPULATION_COLUMN], axis=0).sum(axis=0)
-    return nodes_gdf
+    nodes_df = pd.DataFrame(index=acc_mx.columns)
+    nodes_df[ATTRACTIVENESS_COLUMN] = weights_mx.mul(blocks_df[ATTRACTIVENESS_COLUMN], axis=0).sum(axis=0)
+    nodes_df[POPULATION_COLUMN] = weights_mx.mul(blocks_df[POPULATION_COLUMN], axis=0).sum(axis=0)
+    return nodes_df
 
 
 def _calculate_diversity(blocks_df: pd.DataFrame, services_dfs: list[pd.DataFrame]) -> pd.DataFrame:
@@ -69,19 +66,28 @@ def _calculate_attractiveness(blocks_df: pd.DataFrame, lu_consts: dict[LandUse, 
     return blocks_df
 
 
-def _calculate_od_mx(nodes_gdf: gpd.GeoDataFrame, acc_mx: pd.DataFrame) -> pd.DataFrame:
+def _calculate_od_mx(nodes_df: pd.DataFrame, acc_mx: pd.DataFrame) -> pd.DataFrame:
     logger.info("Calculating origin destination matrix")
     acc_mx = acc_mx.replace(0, np.nan)
     return pd.DataFrame(
-        np.outer(nodes_gdf[POPULATION_COLUMN], nodes_gdf[ATTRACTIVENESS_COLUMN]) / acc_mx,
+        np.outer(nodes_df[POPULATION_COLUMN], nodes_df[ATTRACTIVENESS_COLUMN]) / acc_mx,
         index=acc_mx.index,
         columns=acc_mx.columns,
     ).fillna(0.0)
 
 
+def _validate_input(blocks_df: pd.DataFrame, blocks_to_nodes_mx: pd.DataFrame, nodes_to_nodes_mx: pd.DataFrame):
+    logger.info("Validating input data")
+    if not all(blocks_df.index == blocks_to_nodes_mx.index):
+        raise ValueError("blocks_df index and blocks_to_nodes_mx index must match")
+    if not all(blocks_to_nodes_mx.columns == nodes_to_nodes_mx.index):
+        raise ValueError("blocks_to_nodes_mx columns and nodes_to_nodes index must match")
+    if not all(nodes_to_nodes_mx.index == nodes_to_nodes_mx.columns):
+        raise ValueError("nodes_to_nodes_mx index and columns must match")
+
+
 def origin_destination_matrix(
     blocks_df: pd.DataFrame,
-    nodes_gdf: gpd.GeoDataFrame,
     blocks_to_nodes_mx: pd.DataFrame,
     nodes_to_nodes_mx: pd.DataFrame,
     services_dfs: list[pd.DataFrame],
@@ -89,11 +95,8 @@ def origin_destination_matrix(
     lu_consts: dict[LandUse, float] = LU_CONSTS,
 ) -> pd.DataFrame:
 
-    logger.info("Validating input data")
     blocks_df = BlocksSchema(blocks_df)
-    # TODO validate_accessibility_matrix(blocks_to_nodes_acc_mx, blocks_gdf)
-    nodes_gdf = NodesSchema(nodes_gdf)
-    # TODO validate_accessibility_matrix(nodes_to_nodes_acc_mx, nodes_gdf)
+    _validate_input(blocks_df, blocks_to_nodes_mx, nodes_to_nodes_mx)
 
     blocks_df = _calculate_diversity(blocks_df, services_dfs)
     blocks_df = _calculate_attractiveness(blocks_df, lu_consts)
@@ -103,6 +106,6 @@ def origin_destination_matrix(
         blocks_df[[POPULATION_COLUMN, ATTRACTIVENESS_COLUMN]]
     )
 
-    nodes_gdf = _calculate_nodes_weights(blocks_df, nodes_gdf, blocks_to_nodes_mx, accessibility)
+    nodes_gdf = _calculate_nodes_weights(blocks_df, blocks_to_nodes_mx, accessibility)
 
     return _calculate_od_mx(nodes_gdf, nodes_to_nodes_mx)