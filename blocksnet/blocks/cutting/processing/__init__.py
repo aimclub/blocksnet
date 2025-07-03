f852a4d16f0692acb684d7649d66d9362fee346a
Vasiliy Starikov <42021985+vasilstar97@users.noreply.github.com>
2025-07-03 15:13:43 +0300

    Revert "Advanced block cutter"

--- blocksnet/blocks/cutting/processing/__init__.py
+++ blocksnet/blocks/cutting/processing/__init__.py
@@ -1,6 +1,1 @@
 from .core import cut_urban_blocks
-from .utils import make_convex_hulls, clusters
-from .utils import extend_roads_to_boundary
-from .utils import new_borderline
-from .utils import merge_empty_blocks, merge_invalid_blocks
-
