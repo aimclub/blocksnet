f73c7acf6308c5be084ef9090a0e40755aaf930b
Roman <roma.bashirov.1@yandex.ru>
2025-08-05 17:39:00 +0300

    Refactor and implement sklearn voting ensemble strategies

--- blocksnet/machine_learning/strategy/sklearn/ensemble/voting/base_strategy.py
+++ blocksnet/machine_learning/strategy/sklearn/ensemble/voting/base_strategy.py
@@ -1,7 +1,13 @@
 from abc import ABC
 from ..base_strategy import SKLearnEnsembleBaseStrategy
-
+import numpy as np
 
 class SKLearnVotingBaseStrategy(SKLearnEnsembleBaseStrategy, ABC):
 
-    pass
+    def train(self, x_train : np.ndarray, y_train : np.ndarray, x_test : np.ndarray, y_test : np.ndarray) -> float:
+        self.model = self.model_cls(estimators=self.estimators, **self.model_params)
+        self.model.fit(x_train, y_train)
+        return self.model.score(x_test, y_test)
+    
+    def predict(self, x):
+        return self.model.predict(x)
--- blocksnet/machine_learning/strategy/sklearn/ensemble/voting/classification_strategy.py
+++ blocksnet/machine_learning/strategy/sklearn/ensemble/voting/classification_strategy.py
@@ -1,6 +1,14 @@
+from sklearn.ensemble import VotingClassifier
 from .base_strategy import SKLearnVotingBaseStrategy
 from blocksnet.machine_learning.strategy import ClassificationBase
+from sklearn.base import BaseEstimator
+import numpy as np
 
+class SKLearnVotingClassificationStrategy(SKLearnVotingBaseStrategy, ClassificationBase):
 
-class SKLearnClassificationStrategy(SKLearnVotingBaseStrategy, ClassificationBase):
-    pass
+    def __init__(self, estimators : list[tuple[str, BaseEstimator]], model_params: dict | None = None):
+        super().__init__(VotingClassifier, estimators, model_params)
+
+    def predict_proba(self, x):
+        return self.model.predict_proba(x)
+
