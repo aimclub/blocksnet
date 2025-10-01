3fd74cf2cf6467e8db728e6145e001f107ace159
Vasiliy Starikov <42021985+vasilstar97@users.noreply.github.com>
2025-10-01 10:01:25 +0300

    Ensure docs workflow prepares notebook execution environment

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -47,6 +47,7 @@ extensions = [
 ]
 
 autosummary_generate = True
+nbsphinx_execute = "never"
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
